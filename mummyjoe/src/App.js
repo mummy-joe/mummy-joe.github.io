ca4dfc865d6529baf9efb6ff0605495c611ec673
ahmadkady <joe@mummyjoe.com>
2023-10-20 19:06:04 -0400

    mummy joe is online

---
+++ mummyjoe/src/App.js
@@ -0,0 +1,14 @@
+import header from './header.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={header} alt="mummy joe is online" className="Header-img"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
